db7ca294e8da6b442f535661450deecc3cb4b4c8
samuel-eric <samueltan086@gmail.com>
2023-12-06 17:11:52 +0700

    ubah2 dikit

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const app = express();
-const catalogueRoutes = require('./src/routes/catalogue');
+const app = express()
+const catalogueRoutes = require('./src/routes/catalogue')
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+	)
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+	next()
 })
 
-app.use(express.json());
+app.use(express.json())
 
-app.use('/v1/catalogue', catalogueRoutes);
+app.use('/v1/catalogue', catalogueRoutes)
 
 app.use((error, req, res, next) => {
-    const status = error.errorStatur || 500;
-    const message = error.message;
-    const data = error.data;
+	const status = error.errorStatur || 500
+	const message = error.message
+	const data = error.data
 
-    res.status(status).json({message: message, data: data});
-});
+	res.status(status).json({ message: message, data: data })
+})
 
-mongoose.connect('mongodb+srv://aliefnp:eKqfkFEA3IZa7ijT@cluster0.pbrnbbo.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => {
-        app.listen(4000, () => console.log('Connection Success'));
-    })
-    .catch(err => console.log(err));
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		app.listen(4000, () => console.log('Connection Success'))
+	})
+	.catch((err) => console.log(err))
